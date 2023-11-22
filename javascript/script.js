a85a751524ba74f7e1a37433f1f51918f37df3fb
Joan Nadal <jng150@uoc.edu>
2023-11-22 23:56:11 +0100

    update project

--- javascript/script.js
+++ javascript/script.js
@@ -1,5 +1,33 @@
 import axios from 'axios';
 
+let currentSlide = 0;
+
+function showSlide(index) {
+    const carousel = document.getElementById("carousel");
+    const totalSlides = document.querySelectorAll(".carousel-item").length;
+
+    if (index < 0) {
+        index = totalSlides - 1;
+    } else if (index >= totalSlides) {
+        index = 0;
+    }
+    currentSlide = index;
+    carousel.style.transform = `translateX(${-index * 100}%)`;
+}
+
+let prevSlide = document.getElementById("prev");
+prevSlide.addEventListener("click", function () {
+    currentSlide--;
+    showSlide(currentSlide)
+});
+
+let nextSlide = document.getElementById("next");
+nextSlide.addEventListener("click", function () {
+    currentSlide++;
+    showSlide(currentSlide)
+});
+
+
 var temporadas = document.getElementsByClassName('temporada-item');
 Array.from(temporadas).forEach(temporada => {
     let id = temporada.getAttribute("id").match(/(\d+)/)[0];
